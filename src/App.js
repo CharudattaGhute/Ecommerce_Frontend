aab62145406311ffbaae0a2a5e178ac1846d6d57
CharudattaGhute <ghutecharudatta@gmail.com>
2024-08-27 12:59:33 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import "./App.css";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import MyNavbar from "./component/Navbar";
+
 import {
   BrowserRouter as Router,
-  Route,
   Routes,
+  Route,
   Navigate,
-  BrowserRouter,
 } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
+      <MyNavbar />
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/navbar/*" element={<MyNavbar />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
