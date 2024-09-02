09910cecdd392011cc9d48f675a40bcc019a7ad9
CharudattaGhute <ghutecharudatta@gmail.com>
2024-09-02 10:23:43 +0530

    cart updated

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -18,7 +18,7 @@ function Cart() {
             },
           }
         );
-        console.log(response.data);
+        console.log("response.data", response.data);
         setProducts(response.data);
       } catch (error) {
         console.error("Error fetching products: ", error);
@@ -33,13 +33,17 @@ function Cart() {
         <Card key={product._id} style={{ width: "18rem", margin: "10px" }}>
           <Card.Img
             variant="top"
-            src={product.imageUrl || "default-image-url.jpg"}
+            src={
+              product.image
+                ? product.image
+                : "http://localhost:5001/uploads/default-image.jpg"
+            }
             alt={product.productname}
           />
           <Card.Body>
             <Card.Title>{product.productname}</Card.Title>
             <Card.Text>{product.description}</Card.Text>
-            <Card.Text>{product.quantity}</Card.Text>
+            <Card.Text>Quantity: {product.quantity}</Card.Text>
             <Button variant="primary">${product.price}</Button>
           </Card.Body>
         </Card>
