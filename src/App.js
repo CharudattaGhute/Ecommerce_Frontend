5e7c125394d288c2d949a5424a3c5f0ba621d171
CharudattaGhute <ghutecharudatta@gmail.com>
2024-09-09 21:35:04 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MyNavbar from "./component/Navbar";
+import React, { useState } from "react";
 
 import {
   BrowserRouter as Router,
@@ -13,14 +14,23 @@ import {
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
   return (
     <Router>
       <MyNavbar />
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
-        <Route path="/navbar" element={<MyNavbar />} />
+        <Route path="/dashboard/*" element={<Dashboard cart={cart} />} />
+        <Route
+          path="/navbar"
+          element={
+            <MyNavbar
+              cartCount={cart.reduce((total, item) => total + item.quantity, 0)}
+            />
+          }
+        />
       </Routes>
     </Router>
   );
